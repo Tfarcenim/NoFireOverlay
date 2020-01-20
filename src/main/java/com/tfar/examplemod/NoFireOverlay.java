50e9434f56139a9597512b9baeb43cf061ca4226
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-01-19 22:00:37 -0500

    Initial commit

---
+++ src/main/java/com/tfar/examplemod/NoFireOverlay.java
@@ -0,0 +1,24 @@
+package com.tfar.examplemod;
+
+import net.minecraft.potion.Effects;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderBlockOverlayEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(NoFireOverlay.MODID)
+public class NoFireOverlay
+{
+    public static final String MODID = "nofireoverlay";
+
+    public NoFireOverlay() {
+        DistExecutor.runWhenOn(Dist.CLIENT,() -> () -> MinecraftForge.EVENT_BUS.addListener(this::no));
+    }
+    private void no(final RenderBlockOverlayEvent event) {
+        if (event.getPlayer().isPotionActive(Effects.FIRE_RESISTANCE) && event.getPlayer().isBurning()){
+            event.setCanceled(true);
+        }
+    }
+}
